80015660177cfde1969bc9bae31c74801d89ef1e
eagarciah707 <eagarciah707@gmail.com>
2025-06-22 09:08:35 -0500

    feat(dev): add jackson and JsonIgnoreProperties

--- src/main/java/com/andevs/marketplace/function/model/ProvisioningInput.java
+++ src/main/java/com/andevs/marketplace/function/model/ProvisioningInput.java
@@ -1,4 +1,7 @@
 package com.andevs.marketplace.function.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ProvisioningInput(String clientName) {
 }
