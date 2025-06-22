8bdcf2c23af115c645a9c8c3af94f4f9c26cdc79
eagarciah707 <eagarciah707@gmail.com>
2025-06-22 08:48:28 -0500

    feat(dev): add logs

--- src/main/java/com/andevs/marketplace/function/HelloWorldFunction.java
+++ src/main/java/com/andevs/marketplace/function/HelloWorldFunction.java
@@ -1,6 +1,7 @@
 package com.andevs.marketplace.function;
 
 import com.google.cloud.functions.CloudEventsFunction;
+import com.google.events.cloud.firestore.v1.DocumentEventData;
 import com.google.gson.Gson;
 import io.cloudevents.CloudEvent;
 
@@ -10,6 +11,8 @@ public class HelloWorldFunction implements CloudEventsFunction {
     public void accept(CloudEvent event) throws Exception {
 
         System.out.println("New event received: " + event);
+        DocumentEventData firestoreEventData = DocumentEventData.parseFrom(event.getData().toBytes());
+        System.out.println("firestoreEventData: " + firestoreEventData);
         System.out.println("Event type: " + event.getType());
         System.out.println("Event source: " + event.getSource());
         System.out.println("Event data: " + new String(event.getData().toBytes()));
